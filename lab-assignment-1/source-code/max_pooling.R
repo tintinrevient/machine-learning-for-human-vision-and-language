16671f6f1479b46fa2d431bb0ea7dae8f2aae620
Luka van der Plas <43678097+lukavdplas@users.noreply.github.com>
2019-09-20 13:54:05 +0200

    Update max_pooling.R

--- lab-assignment-1/source-code/max_pooling.R
+++ lab-assignment-1/source-code/max_pooling.R
@@ -1,26 +1,36 @@
 maxPooling <- function(input, max_pooling)
 {
-  #pre-process the input/pooling data to ensure they have the right shape
-  if(dim(input)[1] != dim(input)[2])
-    stop("Input should be of shape (x, x), where x = x.")
+  #add padding if image size is not a multiple of pooling size
   
-  if(dim(max_pooling)[1] != dim(max_pooling)[2])
-    stop("Max pooling should be of shape (y, y), where y = y.")
-  
+  #... for number of rows
   if(dim(input)[1] %% dim(max_pooling)[1] != 0)
-    stop("If the shape of input is (x, x) and the shape of max pooling is (y, y), (x % y) shoud be 0.")
+  {
+    difference <- dim(input)[1] %% dim(max_pooling)[1]
+    padding <- matrix(0, ncol=dim(input)[2], nrow=difference)
+    input <- rbind(input, padding)
+  }
+  
+  #... for number of columns
+  if(dim(input)[2] %% dim(max_pooling)[2] != 0)
+  {
+    difference <- dim(input)[2] %% dim(max_pooling)[2]
+    padding <- matrix(0, nrow=dim(input)[1], ncol=difference)
+    input <- cbind(input, padding)
+  }
+
   
   #strides which the max pooling takes to move along x-axis and y-axis
-  stride <- dim(max_pooling)[1]
+  stride_x <- dim(max_pooling)[1]
+  stride_y <- dim(max_pooling)[2]
   
   #steps which the max pooling takes to move along x-axis
   x_steps <- dim(input)[1] / dim(max_pooling)[1]
   
   #steps which the max pooling takes to move along y-axis
-  y_steps <- x_steps
+  y_steps <- dim(input)[2] / dim(max_pooling)[2]
   
   #output returned by the max pooling function
-  output <- matrix(0, nrow=y_steps, ncol=x_steps)
+  output <- matrix(0, nrow=x_steps, ncol=y_steps)
   
   #initial position in the output matrix, which is (0, 0)
   x <- 1
@@ -31,12 +41,13 @@ maxPooling <- function(input, max_pooling)
     for(j in seq(y_steps))
     {
       #matrix position x from input, as to where to crop the input
-      pos_x <- 1 + stride*(j-1)
+      pos_x <- 1 + stride_x*(j-1)
       
       #matrix position y from input, as to where to crop the input
-      pos_y <- 1 + stride*(i-1)
+      pos_y <- 1 + stride_y*(i-1)
       
-      output[x,y] <- max(input[pos_x:(pos_x+stride-1), pos_y:(pos_y+stride-1)])
+
+      output[y,x] <- max(input[pos_y:(pos_y+stride_y-1), pos_x:(pos_x+stride_x-1)])
       x <- x + 1
     }
     y <- y + 1
@@ -47,9 +58,8 @@ maxPooling <- function(input, max_pooling)
   output
 }
 
+
 #testing
-input <- matrix(round(runif(16, min=0, max=10)), nrow=4, ncol=4)
+input <- matrix(round(runif(20, min=0, max=10)), nrow=4, ncol=5)
 max_pooling <- matrix(0, nrow=2, ncol=2)
 output <- maxPooling(input, max_pooling)
-
-
